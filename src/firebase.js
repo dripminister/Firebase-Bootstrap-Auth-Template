00807c0b702bfbf325b65942482c4bc6aad9f5ec
dripminister <95714820+dripminister@users.noreply.github.com>
2022-12-20 09:28:00 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAvBlw1RHp7MXcnQp5ul9-VjFp0uo4tcNg",
-	authDomain: "drive-clone-e2ca6.firebaseapp.com",
-	projectId: "drive-clone-e2ca6",
-	storageBucket: "drive-clone-e2ca6.appspot.com",
-	messagingSenderId: "421590226067",
-	appId: "1:421590226067:web:41385791154c0555002966",
+//Firebase config
 }
 
 const app = initializeApp(firebaseConfig)
